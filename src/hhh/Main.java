d7fd0e8653ec635e83c2fa80ad721044447188e5
Elmira <elmirasco@gmail.com>
2019-03-13 14:20:34 +0200

    Add System_out in Main class

--- src/hhh/Main.java
+++ src/hhh/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("hhhhhhhhhhh");
         System.out.println("jjjjjjjjjjjjjj");
+        System.out.println("Hello-hello");
+
 
     }
 }
